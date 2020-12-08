2876476a27478dbeb50f241fc2782103ca2571fb
s.amara <s.amara@student.isartdigital.com>
2020-12-08 14:34:16 +0100

    Script to show instruction on a button click, and to chage the same button's text to "Hide"

---
+++ Assets/Scripts/UI/InstructionsText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionsText : MonoBehaviour
+{
+    [SerializeField]
+    Text    buttonText      = null;
+
+    [SerializeField]
+    Text    instructions    = null;
+
+    bool    shown           = false;
+
+    void Start()
+    {
+        instructions.enabled = false;
+    }
+
+    // Update is called once per frame
+    public void ToggleInstructions()
+    {
+        shown = !shown;
+        buttonText.text = shown ? "Hide instructions" : "Show instructions";
+        instructions.enabled = shown;
+    }
+}
