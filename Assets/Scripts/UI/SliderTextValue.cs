51941db64129a2097b3dbbaa7cd346f3ef311a66
s.amara <s.amara@student.isartdigital.com>
2020-12-08 14:34:57 +0100

    Script which updates a passed text box with the value of a slider

---
+++ Assets/Scripts/UI/SliderTextValue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderTextValue : MonoBehaviour
+{
+    [SerializeField]
+    Slider  slider = null;
+    Text    text;
+
+    void Start()
+    {
+        text    = GetComponent<Text>();
+        slider  = GetComponentInParent<Slider>();
+
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        text.text = slider.value.ToString();
+    }
+}
