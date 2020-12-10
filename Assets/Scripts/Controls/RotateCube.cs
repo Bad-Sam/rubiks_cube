a7ff12c4989a12a215d941a074f603edbe088f28
Bad-Sam <s.amara@student.isartdigital.com>
2020-12-10 10:50:46 +0100

    Defines a rotation axis and an angle to rotate the Rubik's Cube, taking into account the camera transform, the Rubik's transform, and the mouse movements when the right click is pressed

---
+++ Assets/Scripts/Controls/RotateCube.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    /* ==== Inspector-accessible variables ==== */
+    [SerializeField]
+    private Transform rubiksTransform = null;
+
+    [SerializeField]
+    [Tooltip("Angular velocity or the cube's rotation")]
+    [Range(.01f, 10f)]
+    private float rotSpeed = .01f;
+
+    /* ==== Internal variables ==== */
+    private Transform camTransform = null;
+    private Vector3 rotAxis;
+    
+
+    /* ==== Methods ==== */
+    void Start()
+    {
+        Debug.Assert(rubiksTransform != null);
+        camTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            // Define an axis in the plane of the camera
+            rotAxis = Input.GetAxis("Mouse X") * camTransform.right + Input.GetAxis("Mouse Y") * camTransform.up;
+
+            // Rotate it by 90° in the plane of the camera, to make the cube rotate in the same direction as the mouse
+            rotAxis.Set(rotAxis.y, -rotAxis.x, rotAxis.z);
+
+            // Express this axis into the cube's referencial
+            rotAxis.Set(Vector3.Dot(rotAxis, rubiksTransform.transform.right),
+                        Vector3.Dot(rotAxis, rubiksTransform.transform.up),
+                        Vector3.Dot(rotAxis, rubiksTransform.transform.forward));
+            
+            // Apply the rotation, taking into account the intensity of the last mouse movement
+            rubiksTransform.transform.rotation *= Quaternion.AngleAxis(rotAxis.magnitude * rotSpeed, Vector3.Normalize(rotAxis));
+        }
+    }
+}
