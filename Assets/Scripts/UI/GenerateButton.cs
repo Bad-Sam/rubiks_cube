04f561650248f75d9716da1f8ea2c9be816e6de9
Bad-Sam <s.amara@student.isartdigital.com>
2020-12-12 20:48:48 +0100

    Link between the 2 UI sliders, the generate button, and the Rubik's Cube regeneration with the UI. This generates a new Rubik's Cube

---
+++ Assets/Scripts/UI/GenerateButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateButton : MonoBehaviour
+{
+    [SerializeField]
+    Slider sizeSlider = null;
+
+    [SerializeField]
+    Slider shuffleSlider = null;
+
+    [SerializeField]
+    RubiksCube rubiksCube = null; 
+
+    void Awake()
+    {
+        Debug.Assert(sizeSlider != null);
+        Debug.Assert(shuffleSlider != null);
+        Debug.Assert(rubiksCube != null);
+    }
+
+    
+    public void UpdateRubiksCube()
+    {
+        rubiksCube.Generate((int)Mathf.Round(sizeSlider.value), (int)Mathf.Round(shuffleSlider.value));
+    }
+}
