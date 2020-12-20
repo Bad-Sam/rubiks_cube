fc13ca25d81c4a7b811ece71f9a1843a91beb59c
Bad-Sam <s.amara@student.isartdigital.com>
2020-12-20 16:04:12 +0100

    Script attached to the text which appears on completion of the Rubik's Cube

---
+++ Assets/Scripts/UI/CubeSolved.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeSolved : MonoBehaviour
+{
+    private Text text = null;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        text.color = new Color(.902f, .6275f, .2118f, 1f);
+        text.CrossFadeAlpha(0f, 0f, true);
+    }
+
+    public void Trigger()
+    {
+        text.CrossFadeAlpha(1f, 0f, true);
+        text.CrossFadeAlpha(0f, 3f, true);
+    }
+}
