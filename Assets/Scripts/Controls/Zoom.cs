39099caad6329d48eb95de3a3e8fe3377f5f8f63
Bad-Sam <s.amara@student.isartdigital.com>
2020-12-20 21:39:45 +0100

    Zoom in/out with the mouse's wheel

---
+++ Assets/Scripts/Controls/Zoom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+    private Transform camTransform = null;
+
+    void Start()
+    {
+        camTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        camTransform.position += Input.mouseScrollDelta.y * camTransform.forward;
+    }
+}
