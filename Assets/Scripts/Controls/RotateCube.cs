b750c9e2046d0d5c2b3ca06c1dce29e4c348656e
Bad-Sam <s.amara@student.isartdigital.com>
2021-01-17 11:01:33 +0100

    The transition toward the new orientation generated by a mouse drag is now transitionned to with a spherical interpolation

--- Assets/Scripts/Controls/RotateCube.cs
+++ Assets/Scripts/Controls/RotateCube.cs
@@ -11,14 +11,27 @@ public class RotateCube : MonoBehaviour
     [Range(1f, 10f)]
     private float rotSpeed = .01f;
 
+    [SerializeField]
+    [Tooltip("Strength of the spherical interpolation between 2 rotations")]
+    [Range(0f, 1f)]
+    private float slerpStrength = .175f;
+
     /* ==== Internal variables ==== */
     private Transform   camTransform = null;
+    private Quaternion  targetRot;
     private Vector3     rotAxis;
     public  bool        allowFullCubeRotation = true;
 
     /* ==== Methods ==== */
     void Start()
     {
+        SavedData save = GameManager.GetSave();
+
+        if (save != null)
+            targetRot = new Quaternion(save.rubiksPosRot.rot[0], save.rubiksPosRot.rot[1], save.rubiksPosRot.rot[2], save.rubiksPosRot.rot[3]);
+        else
+            targetRot = transform.rotation;
+
         camTransform = Camera.main.transform;
     }
 
@@ -26,19 +39,21 @@ public class RotateCube : MonoBehaviour
     {
         if (Input.GetMouseButton(1) && allowFullCubeRotation)
         {
-            // Define an axis in the far plane of the camera
+            // Define an axis in the plane of the camera
             rotAxis = Input.GetAxis("Mouse X") * camTransform.right + Input.GetAxis("Mouse Y") * camTransform.up;
 
             // Rotate it by 90° in the plane of the camera, to make the cube rotate in the same direction as the mouse
             rotAxis.Set(rotAxis.y, -rotAxis.x, rotAxis.z);
 
             // Express this axis into the cube's referential
-            rotAxis.Set(Vector3.Dot(rotAxis, rubiksTransform.transform.right),
-                        Vector3.Dot(rotAxis, rubiksTransform.transform.up),
-                        Vector3.Dot(rotAxis, rubiksTransform.transform.forward));
+            rotAxis.Set(Vector3.Dot(rotAxis, rubiksTransform.right),
+                        Vector3.Dot(rotAxis, rubiksTransform.up),
+                        Vector3.Dot(rotAxis, rubiksTransform.forward));
             
             // Apply the rotation, taking into account the intensity of the last mouse movement
-            rubiksTransform.transform.rotation *= Quaternion.AngleAxis(rotAxis.magnitude * rotSpeed, Vector3.Normalize(rotAxis));
+            targetRot *= Quaternion.AngleAxis(rotAxis.magnitude * rotSpeed, Vector3.Normalize(rotAxis));
         }
+
+        rubiksTransform.rotation = Quaternion.Slerp(rubiksTransform.rotation, targetRot, slerpStrength);
     }
 }
